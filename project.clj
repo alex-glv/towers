0ad0e2433a1f55814bc3285d53fff83c853890c2
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2014-11-12 23:01:30 +0100

    Adding nrepl support w/ austin; adding server-start / stop functions

--- project.clj
+++ project.clj
@@ -6,11 +6,14 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [compojure "1.2.1"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [ring-server "0.3.1"]
+                 [jayq "2.5.2"]]
     
   :source-paths ["src/clj" "src/cljs"]
   :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-ring "0.8.12"]]
+            [lein-ring "0.8.12"]
+            [com.cemerick/austin "0.1.5"]]
   :ring {:handler towers.core/handler}
   :cljsbuild {:builds
               [{;; CLJS source code path
@@ -18,8 +21,8 @@
                 ;; Google Closure (CLS) options configuration
                 :compiler {;; CLS generated JS script filename
                            :output-to "resources/public/js/towers.js"
+                           :externs ["resources/public/js/externs/jquery-2.1.1.js"]
                            ;; minimal JS optimization directive
-                           :optimizations :none
-                           :source-map true
+                           :optimizations :whitespace
                            ;; generated JS code prettyfication
                            :pretty-print true}}]})
--- src/clj/towers/core.clj
+++ src/clj/towers/core.clj
@@ -1,7 +1,17 @@
 (ns towers.core
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.server.standalone :as ring-server]
+            [ring.middleware file-info file]
+            ))
+(def repl-env (reset! cemerick.austin.repls/browser-repl-env
+                      (cemerick.austin/repl-env)))
+
+(defn start-cljs-repl
+  ([handler] (cemerick.austin.repls/cljs-repl handler))
+  ([] (start-cljs-repl repl-env)))
+
 (defroutes app-routes
   ; to serve document root address
   (GET "/" [] "<p>Hello from compojure</p>")
@@ -15,3 +25,21 @@
 (def handler
   (handler/site app-routes))
 
+(defonce server (atom nil))
+
+(defn server-start
+  "used for starting the server in development mode from REPL"
+  [& [port]]
+  (let [port (if port (Integer/parseInt port) 3000)]
+    (reset! server
+            (ring-server/serve handler
+                   {:port port
+                    :open-browser? false
+                    :auto-reload? true
+                    :join true}))
+    (println (cemerick.austin.repls/browser-connected-repl-js))
+    (println (str  "You can view the site at http://localhost:" port))))
+
+(defn server-stop []
+  (.stop @server)
+  (reset! server nil))
